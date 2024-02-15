79eb4309c985a80a84cc5676e226bbc3856a10a8
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-15 12:06:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 import streamlit as st
 
-st.title('Hello world!')
+st.title('Menú cíclico')
 
-st.header('header')
-st.text('text2')
+st.header('Autor: Gabriel Barragán')
 
-st_name = st.sidebar.text_input('Enter your name: ','Gabriel')
-
-st.write('Hello', st_name, '!')
+data = pd.DataFrame({
+  'Presion_atmosferica': [993,994,997,1003,1004,1000,994,942,1006,942,986,083,940,966,982],
+  'Velocidad_viento': [50,60,45,45,40,55,55,105,40,120,50,70,120,100,55]
+})
+data.description = '''Hurricanes The data to the right represent the atmospheric
+pressure p (in millibars) and the wind speed w (in knots)
+measured during various tropical systems in the Atlantic
+Ocean'''
+st.write(data.description)
