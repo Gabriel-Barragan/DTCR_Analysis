2d64c66e5064a4c406fd50bc473ca70bbde4c1a8
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-15 11:42:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 import streamlit as st
 
 st.title('Hello world!')
@@ -8,5 +12,4 @@ st.text('text2')
 
 st_name = st.sidebar.text_input('Enter your name: ','Gabriel')
 
-#st.write(f'Hello {st_name}!')
 st.write('Hello', st_name, '!')
