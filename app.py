eb59ca3916e4cf0ed4495949f34b06f92e23d178
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-15 12:07:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ data = pd.DataFrame({
   'Presion_atmosferica': [993,994,997,1003,1004,1000,994,942,1006,942,986,983,940,966,982],
   'Velocidad_viento': [50,60,45,45,40,55,55,105,40,120,50,70,120,100,55]
 })
-data.description = '''Hurricanes The data to the right represent the atmospheric
+data.description = '''Hurricanes: The data to the right represent the atmospheric
 pressure p (in millibars) and the wind speed w (in knots)
 measured during various tropical systems in the Atlantic
 Ocean'''
