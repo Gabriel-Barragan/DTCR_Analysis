9d344b22a046472f21d2d4295160a0b0c9de6b12
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-16 17:34:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ elif option == 'Features':
   # https://www.ncbi.nlm.nih.gov/books/NBK65719.15/table/  
   
   for col in data.columns:
-    st.write(f"Feature: {col})
+    st.write(f"Feature: {col}")
     st.write(f"Description: {data_descriptions[col]}")
     
   st.write('# Type of features:')
