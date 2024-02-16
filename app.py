dc2992033100e89a22fee650fe81e08f6e24b3a2
Gabriel-Barragan <69125040+Gabriel-Barragan@users.noreply.github.com>
2024-02-16 14:23:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ data = pd.read_csv('Datasets/Thyroid_Diff.csv')
 data.description = '''This data set contains 13 clinicopathologic features aiming to predict recurrence of well differentiated thyroid cancer.
 The data set was collected in duration of 15 years and each patient was followed for at least 10 years.'''
 
-option = st.radio('Select an option: ', ['Description','Display'])
+option = st.radio('Select an option: ', ['Description','Display','Features'])
 
 if option == 'Description':
   st.write(data.description)
@@ -30,3 +30,7 @@ elif option == 'Display':
   st.subheader(f"{number_sample_intances} Random sample of instances:")
   st.write(data.sample(number_sample_intances))
 
+elif option == 'Features':
+  st.write('Dimension of data set DTCR: %d instances and %d features' %(data.shape[0], data.shape[1]))
+  for col in data.columns:
+    st.write(col)
